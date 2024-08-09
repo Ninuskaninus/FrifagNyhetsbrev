06d9346ee280cd9bc7fc85d5c94717120a1542ad
Ninuskaninus <89078328+Ninuskaninus@users.noreply.github.com>
2024-08-09 14:00:44 +0200

    fjernet boks på toppen

--- src/components/heading/index.styles.jsx
+++ src/components/heading/index.styles.jsx
@@ -2,14 +2,11 @@ import styled from "styled-components";
 
 export const TopItem = styled.div`
 h1{
-  margin-bottom: 1.5rem;
+  margin-bottom: 1rem;
 }
 `;
 
 export const TopInfo = styled.div`
-background-color: var(--white);
-box-shadow: var(--boxShadow);
-padding: 4rem;
-width: 100%;
+
 
 `;
